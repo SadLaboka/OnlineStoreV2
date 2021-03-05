1687c09cec4efe885810ff48b8bc33f0b6e1ef7e
SadLaboka <toteshephysic@yandex.ru>
2021-03-05 21:41:00 +0300

    Добавил пагинацию

--- OnlineStore/settings.py
+++ OnlineStore/settings.py
@@ -137,4 +137,9 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static_dev'),
 )
 
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 2
+}
+
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
