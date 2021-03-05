07ce6c1d8e72949ae5df967fde1792980370e528
SadLaboka <toteshephysic@yandex.ru>
2021-03-05 21:27:16 +0300

    Добавил вывод информации о конкретном товаре и список покупателей в API

--- main/api/api_views.py
+++ main/api/api_views.py
@@ -1,8 +1,13 @@
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView
 from rest_framework.filters import SearchFilter
 
-from .serializers import CategorySerializer, SmartphoneSerializer, NotebookSerializer
-from ..models import Category, Smartphone, Notebook
+from .serializers import (
+    CategorySerializer,
+    SmartphoneSerializer,
+    NotebookSerializer,
+    CustomerSerializer,
+)
+from ..models import Category, Smartphone, Notebook, Customer
 
 
 class CategoryListAPIView(ListAPIView):
@@ -25,3 +30,23 @@ class NotebookListAPIView(ListAPIView):
     queryset = Notebook.objects.all()
     filter_backends = [SearchFilter]
     search_fields = ['price', 'title']
+
+
+class SmartphoneDetailAPIView(RetrieveAPIView):
+
+    serializer_class = SmartphoneSerializer
+    queryset = Smartphone.objects.all()
+    lookup_field = 'slug'
+
+
+class NotebookDetailAPIView(RetrieveAPIView):
+
+    serializer_class = NotebookSerializer
+    queryset = Notebook.objects.all()
+    lookup_field = 'slug'
+
+
+class CustomersListAPIView(ListAPIView):
+
+    serializer_class = CustomerSerializer
+    queryset = Customer.objects.all()
