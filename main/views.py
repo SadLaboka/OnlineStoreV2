e320efc2eef05c39cbc7e04dc0031a4d265fbc95
SadLaboka <toteshephysic@yandex.ru>
2021-03-07 22:30:08 +0300

    Добавил профиль пользователя

--- main/views.py
+++ main/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth import authenticate, login
 from django.http import HttpResponseRedirect
 from django.views.generic import DetailView, View
 
-from .models import Category, Customer, Cart, CartProduct, Product
+from .models import Category, Customer, Cart, CartProduct, Product, Order
 from .mixins import CartMixin
 from .forms import OrderForm, LoginForm, RegistrationForm
 from .utils import recalc_cart
@@ -200,3 +200,16 @@ class RegistrationView(CartMixin, View):
             return HttpResponseRedirect('/')
         context = {'form': form, 'cart': self.cart}
         return render(request, 'main/registration.html', context)
+
+
+class ProfileView(CartMixin, View):
+
+    def get(self, request, *args, **kwargs):
+        customer = Customer.objects.get(user=request.user)
+        orders = Order.objects.filter(customer=customer).order_by('-created_at')
+        categories =Category.objects.all()
+        return render(
+            request,
+            'main/profile.html',
+            {'orders': orders, 'cart': self.cart, 'categories': categories}
+        )
