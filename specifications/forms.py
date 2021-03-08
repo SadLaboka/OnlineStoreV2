ffeaa5cfa000c9fb29286b15637712c31453a0fd
SadLaboka <toteshephysic@yandex.ru>
2021-03-08 22:27:19 +0300

    Добавил возможность добавления новых типов характеристик для определенной категории со страницы для управления характеристиками

--- specifications/forms.py
+++ specifications/forms.py
@@ -1,10 +1,18 @@
 from django import forms
 
 from shop.models import Category
+from .models import CategoryFeature
 
 
 class NewCategoryForm(forms.ModelForm):
 
     class Meta:
         model = Category
+        fields = '__all__'
+
+
+class NewCategoryFeatureKeyForm(forms.ModelForm):
+
+    class Meta:
+        model = CategoryFeature
         fields = '__all__'
--- specifications/views.py
+++ specifications/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.views import View
 
-from .forms import NewCategoryForm
+from .forms import NewCategoryForm, NewCategoryFeatureKeyForm
 
 
 class BaseSpecView(View):
@@ -25,3 +25,22 @@ class NewCategoryView(View):
             form.save()
             return HttpResponseRedirect('/product-specs/')
         return render(request, 'specifications/new_category.html', context)
+
+
+class CreateNewFeature(View):
+
+    def get(self, request, *args, **kwargs):
+        form = NewCategoryFeatureKeyForm(request.POST or None)
+        context = {'form': form}
+        return render(request, 'specifications/new_feature.html', context)
+
+    def post(self, request, *args, **kwargs):
+        form = NewCategoryFeatureKeyForm(request.POST or None)
+        context = {'form': form}
+        if form.is_valid():
+            new_category_feature_key = form.save(commit=False)
+            new_category_feature_key.category = form.cleaned_data['category']
+            new_category_feature_key.feature_name = form.cleaned_data['feature_name']
+            new_category_feature_key.save()
+            return HttpResponseRedirect('/product-specs/')
+        return render(request, 'specifications/new_feature.html', context)
